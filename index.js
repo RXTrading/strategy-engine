66c1a2f902842aafeeccbc9603ecb0e6b9cf3602
Steve Clarke <116336693+srcmayte@users.noreply.github.com>
2024-03-17 10:56:10 +0000

    Fact descriptions and better types (#2)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const StrategyEngine = require('./lib/strategyEngine')
 const Fact = require('./lib/facts/base')
+const Facts = require('./lib/facts')
 
-module.exports = { StrategyEngine, Fact }
+module.exports = { StrategyEngine, Fact, Facts }
